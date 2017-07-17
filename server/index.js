27ab41fe059a957222d65ac5c4f3e5d0f47a94ac
José Loarca <joseloarca97@icloud.com>
2017-07-16 22:28:28 -0600

    Creando servidor con express y vista para el envio de mensajes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
- 
+var express = require('express');
+
+var app = express();
+var server = require('http').Server(app);
+
+var io = require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/', function(req, res){
+    res.status(200).send('Chat con Socket IO & Node.js');
+});
+
+io.on('connection', function(socket){
+    console.log('Nueva conexión desde: '+socket.handshake.address);
+});
+
+server.listen(6677, function(){
+    console.log('Servidor iniciado en puerto 6677');
+});
