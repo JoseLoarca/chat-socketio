9f1b60bae47ddd2cb498b3888891772d0b9d109a
José Loarca <joseloarca97@icloud.com>
2017-07-16 23:50:51 -0600

    #DONE

--- server/index.js
+++ server/index.js
@@ -11,8 +11,22 @@ app.get('/', function(req, res){
     res.status(200).send('Chat con Socket IO & Node.js');
 });
 
+var messages = [{
+    id: 1,
+    text: 'Chat con Socket IO & Node.js...',
+    nickname: 'Bot - JC'
+}];
+
 io.on('connection', function(socket){
     console.log('Nueva conexión desde: '+socket.handshake.address);
+    
+    socket.emit('messages', messages);
+    
+    socket.on('add-message', function(data){
+        messages.push(data);
+        
+        io.sockets.emit('messages', messages);
+    });
 });
 
 server.listen(6677, function(){
